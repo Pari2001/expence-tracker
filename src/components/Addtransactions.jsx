b0fa869a4e23452bd0dec4082e96aad092097f9a
pari2001 <papri9538@gmail.com>
2022-09-05 18:56:39 +0530

    first commit

---
+++ src/components/Addtransactions.jsx
@@ -0,0 +1,237 @@
+import React, { useEffect, useState } from "react";
+import "../styles/addtransactions.css";
+import moneypig2 from "../images/moneypig.svg";
+import party from "party-js";
+import expend from "../images/expend.svg"
+
+const Addtransactions = () => {
+  const [swi, setSwi] = useState(true);
+  const [popup, setPopup] = useState(false);
+  const [category, setCategory] = useState("Dropdown Button");
+
+  const [reason, setReason] = useState("");
+  const [amount, setAmount] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPopup(false);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [popup === true]);
+
+
+  return (
+    <>
+      <div className="mouse600 flexy page-headings my-4">Add Transactions</div>
+
+      <div className="container-fluid mt-4 question-box px-lg-5 px-3 mouse400">
+        <div class="ui top attached tabular question-discuss-border menu">
+          <span
+            onClick={() => {
+              setSwi(true);
+              setCategory("Dropdown Button");
+              setReason("");
+              setAmount("");
+              setPopup(false);
+            }}
+            className={
+              swi === true
+                ? `discuss-active tabular-button item active`
+                : `discuss-active mouse600 tabular-button item`
+            }
+          >
+            Expenditure
+          </span>
+          <span
+            onClick={() => {
+              setSwi(false);
+              setCategory("Dropdown Button");
+              setReason("");
+              setAmount("");
+              setPopup(false);
+            }}
+            className={
+              swi === false
+                ? `discuss-active mouse600 tabular-button item active`
+                : `discuss-active mouse600 tabular-button item`
+            }
+          >
+            Income
+          </span>
+        </div>
+
+
+        <div className="ui attached segment">
+
+        {
+          popup && (
+          <div class="alert alert-success d-flex align-items-center my-2 mx-2" role="alert">
+          <i class="check icon mb-2"></i>
+          <div>
+          Transaction Added
+          </div>
+          </div>
+          )
+          }
+
+          {swi === true ? (
+            <>
+
+              <div className="mt-4">
+                <label
+                  htmlFor=""
+                  className="d-flex justify-content-start form-label"
+                >
+                  Category
+                </label>
+                <div class="dropdown">
+                  <button class="button ui basic dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                  {category}
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li className="dropdown-item" onClick={()=>{setCategory("Groceries")}}>Groceries</li>
+                    <li className="dropdown-item" onClick={()=>{setCategory("Bills")}}>Bills</li>
+                    <li className="dropdown-item" onClick={()=>{setCategory("Entertainment")}}>Entertainment</li>
+                    <li className="dropdown-item" onClick={()=>{setCategory("Insurance & EMIs")}}>Insurance & EMIs</li>
+                    <li className="dropdown-item" onClick={()=>{setCategory("Educations")}}>Education</li>
+                    <li className="dropdown-item" onClick={()=>{setCategory("Shopping")}}>Shopping</li>
+                    <li className="dropdown-item" onClick={()=>{setCategory("Household Needs")}}>Household Needs</li>
+                    <li className="dropdown-item" onClick={()=>{setCategory("Others")}}>Others</li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <label
+                  htmlFor=""
+                  className="d-flex justify-content-start form-label"
+                >
+                  Reason
+                </label>
+                <input 
+                onChange={(event)=>{setReason(event.target.value)}}
+                placeholder="Add Details" 
+                value={reason} type="text" 
+                className="index-form form-control" />
+              </div>
+
+              <div className="mt-4">
+                <label
+                  htmlFor=""
+                  className="d-flex justify-content-start form-label"
+                >
+                  Amount
+                </label>
+                <input 
+                onChange={(event)=>{setAmount(event.target.value)}}
+                placeholder="Add Amount" 
+                value={amount} type="text" className="index-form form-control" />
+              </div>
+
+
+              <div className="flexy">
+                <button
+                  onClick={() => {
+                    setCategory("Dropdown Button");
+                    setAmount("");
+                    setReason("");
+                    setPopup(true);
+                    // party.confetti(
+                    //   document.querySelector(".add-transaction-btn"),
+                    //   {
+                    //     count: party.variation.range(20, 40)
+                    //   }
+                    // );
+                  }}
+                  className="button add-transaction-btn text-dark curvy-btn ui mt-4"
+                >
+                  Add
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+
+
+              <div className="mt-4">
+                <label
+                  htmlFor=""
+                  className="d-flex justify-content-start form-label"
+                >
+                  Category
+                </label>
+                <div class="dropdown">
+                  <button class="button ui basic dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                  {category}
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li className="dropdown-item" onClick={()=>{setCategory("Salary")}}>Salary</li>
+                    <li className="dropdown-item" onClick={()=>{setCategory("Others")}}>Others</li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <label
+                  htmlFor=""
+                  className="d-flex justify-content-start form-label"
+                >
+                  Reason
+                </label>
+                <input 
+                onChange={(event)=>{setReason(event.target.value)}}
+                placeholder="Add Details" 
+                value={reason} type="text" 
+                className="index-form form-control" />
+              </div>
+
+              <div className="mt-4">
+                <label
+                  htmlFor=""
+                  className="d-flex justify-content-start form-label"
+                >
+                  Amount
+                </label>
+                <input 
+                onChange={(event)=>{setAmount(event.target.value)}}
+                placeholder="Add Amount" 
+                value={amount} type="text" className="index-form form-control" />
+              </div>
+
+              <div className="flexy">
+
+                <button
+                  onClick={() => {
+                    setCategory("Dropdown Button");
+                    setAmount("");
+                    setReason("");
+                    setPopup(true);
+                    // party.confetti(
+                    //   document.querySelector(".add-transaction-btn"),
+                    //   {
+                    //     count: party.variation.range(20, 40)
+                    //   }
+                    // );
+                  }}
+                  className="button add-transaction-btn text-dark curvy-btn ui mt-4"
+                >
+                  Add
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+{
+      // {popup && (
+      //   <div className="popup flexy">
+      //     <img alt="" className="popup-img" src={swi === true ? expend : moneypig2} />
+      //   </div>
+      // )}
+    
+    }
+    </>
+  );
+};
+
+export default Addtransactions;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,169 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import '../styles/navbar.css'
+import Rupee from '../images/rupee.png'
+import { useNavigate } from 'react-router-dom'
+
+const Navbar = (props) => {
+
+    const navigate = useNavigate();
+
+    const logout = () => {
+        props.toggleUser(false)
+        navigate('/signin')
+    }
+
+    return (
+        <>
+
+            <nav className="navbar navbar-expand-md navbar-light">
+                <div className="container-fluid">
+                    <Link exact to="/" className="navbar-brand mouse400 py-2">
+                    <img className='me-2 navbar-logo' src={Rupee} alt="" />
+                    Spending Analyser
+                    </Link>
+
+                    <button
+                        className="navbar-toggler voltorb-navbar-toggler bg-gate"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse mt-2" id="navbarNav">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+
+                            <li className="nav-item">
+                                <NavLink
+                                    exact to={'/'}
+                                    activeclassname="active"
+                                    className="nav-link d-sm-none">
+                                    <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                        Home
+                                    </span>
+                                </NavLink>
+                                <NavLink
+                                    exact to={'/'}
+                                    activeclassname="active"
+                                    className="nav-link d-none d-sm-block">
+                                    <span>
+                                        Home
+                                    </span>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink
+                                    exact to={'/transactions'}
+                                    activeclassname="active"
+                                    className="nav-link d-sm-none">
+                                    <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                        Transactions
+                                    </span>
+                                </NavLink>
+                                <NavLink
+                                    exact to={'/transactions'}
+                                    activeclassname="active"
+                                    className="nav-link d-none d-sm-block">
+                                    <span>
+                                        Transactions
+                                    </span>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink
+                                    exact to={'/add'}
+                                    activeclassname="active"
+                                    className="nav-link d-sm-none">
+                                    <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                        Add Transactions
+                                    </span>
+                                </NavLink>
+                                <NavLink
+                                    exact to={'/add'}
+                                    activeclassname="active"
+                                    className="nav-link d-none d-sm-block">
+                                    <span>
+                                        Add Transactions
+                                    </span>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink
+                                    exact to={'/manage'}
+                                    activeclassname="active"
+                                    className="nav-link d-sm-none">
+                                    <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                        Manage
+                                    </span>
+                                </NavLink>
+                                <NavLink
+                                    exact to={'/manage'}
+                                    activeclassname="active"
+                                    className="nav-link d-none d-sm-block">
+                                    <span>
+                                        Manage
+                                    </span>
+                                </NavLink>
+                            </li>
+
+                            {
+                                props.user === true
+                                    ?
+                                    <li className="nav-item">
+                                        <NavLink
+                                            onClick={() => (logout())}
+                                            exact to={'/signin'}
+                                            activeclassname="active"
+                                            className="nav-link d-sm-none">
+                                            <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                                Logout
+                                            </span>
+                                        </NavLink>
+                                        <NavLink
+                                            onClick={() => (logout())}
+                                            exact to={'/signin'}
+                                            activeclassname="active"
+                                            className="nav-link d-none d-sm-block">
+                                            <span>
+                                                Logout
+                                            </span>
+                                        </NavLink>
+                                    </li>
+                                    :
+                                    <li className="nav-item">
+                                        <NavLink
+                                            exact to={'/signin'}
+                                            activeclassname="active"
+                                            className="nav-link d-sm-none">
+                                            <span data-bs-target="#navbarNav" data-bs-toggle="collapse">
+                                                SignIn
+                                            </span>
+                                        </NavLink>
+                                        <NavLink
+                                            exact to={'/signin'}
+                                            activeclassname="active"
+                                            className="nav-link d-none d-sm-block">
+                                            <span>
+                                                SignIn
+                                            </span>
+                                        </NavLink>
+                                    </li>
+                            }
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
